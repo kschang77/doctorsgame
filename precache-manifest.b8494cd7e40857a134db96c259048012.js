0814731c89490755c438de1cdc255b389663a194
Kasey Chang <kschang77@yahoo.com>
2020-05-13 07:28:05 -0700

    Updates

---
+++ precache-manifest.b8494cd7e40857a134db96c259048012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7113ac35f7868f3880fbddb8e57f12b2",
+    "url": "/doctorsgame/index.html"
+  },
+  {
+    "revision": "d47b74852737b838a250",
+    "url": "/doctorsgame/static/css/main.af0fb8ed.chunk.css"
+  },
+  {
+    "revision": "3558dcde19e6f95ab537",
+    "url": "/doctorsgame/static/js/2.a6bb0fa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/doctorsgame/static/js/2.a6bb0fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47b74852737b838a250",
+    "url": "/doctorsgame/static/js/main.62416715.chunk.js"
+  },
+  {
+    "revision": "fc6cf87fedc77ec96bd1",
+    "url": "/doctorsgame/static/js/runtime-main.9860753e.js"
+  }
+]);
